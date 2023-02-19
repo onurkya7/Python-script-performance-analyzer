bf9eaf4c5f62bec3b6d56b3578332f0fe3ddeb98
onurkya7 <onurkaya2611@gmail.com>
2023-02-19 12:02:44 +0300

    Update

--- test_for_code.py
+++ test_for_code.py
@@ -3,20 +3,30 @@ import psutil
 import threading
 import os
 
+# Ram Default
+top = 0
+for i in range(5):
+    z = int((str(psutil.virtual_memory().used))[:4])
+    time.sleep(0.1)
+    top += z
+bas_ram = str(top/5)[:4]
+    
+# Test Code
 def your_code():
     baslangic =  time.time()
+    # Paste your code here
+    ######################
 
-    #Paste your code here
-    #####################
+
+
     
-
-    #####################
-
+    #######################
+    # Code Time 
     saniye = time.time() - baslangic
-    print("Geçen Toplam Süre: ",saniye)
-
+    print("TIME:",str(saniye)[:5] + " Second")
     os.mkdir('deneme')
 
+# Benchmark for Code
 def bench():
     cpu = []
     ram = []
@@ -25,13 +35,12 @@ def bench():
         mem_percent = (mem_usage / 100.0)
         cpu.append(cpu_percent)
         ram.append(mem_percent)
-
     while True:
         display_usage(psutil.cpu_percent(), psutil.virtual_memory().used)
-        time.sleep(0.5)
+        time.sleep(0.1)
         if os.path.exists('deneme'):
-            print("Ortalama Cpu Kullanimi: ",str(sum(cpu)/len(cpu))[:5])
-            print("Ortalama Ram Kullanimi: ",abs(int(str(sum(ram)/len(ram))[:4]) - int((str(psutil.virtual_memory().used))[:4])))
+            print("CPU: " + " % " + str(sum(cpu)/len(cpu))[:4])
+            print("RAM: ", abs(int(str(sum(ram)/len(ram))[:4]) - int(bas_ram)),"MB")
             os.rmdir("deneme")
             break
                 
